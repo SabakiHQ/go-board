c1f0dbb3e860177c79acc4366bd192e385b35c2d
林鈺錦 <abc1236762@outlook.com>
2020-05-26 00:19:39 +0800

    No semicolons

--- src/GoBoard.d.ts
+++ src/GoBoard.d.ts
@@ -1,31 +1,31 @@
-export type Vertex = [number, number];
+export type Vertex = [number, number]
 export enum Sign {
     White = -1,
     Empty = 0,
     Black = 1,
 }
-export type SignMap = Sign[][];
+export type SignMap = Sign[][]
 
-declare const alpha: string;
+declare const alpha: string
 
-declare function vertexEquals([x1, y1]: Vertex, [x2, y2]: Vertex): boolean;
+declare function vertexEquals([x1, y1]: Vertex, [x2, y2]: Vertex): boolean
 
 declare class GoBoard {
-    public signMap: SignMap;
-    public height: number;
-    public width: number;
+    public signMap: SignMap
+    public height: number
+    public width: number
 
-    private _players: [Sign, Sign];
-    private _captures: [number, number];
-    private _koInfo: {sign: Sign; vertex: Vertex};
+    private _players: [Sign, Sign]
+    private _captures: [number, number]
+    private _koInfo: {sign: Sign; vertex: Vertex}
 
-    constructor(signMap?: SignMap);
-    static fromDimensions(width: number, height?: number): GoBoard;
+    constructor(signMap?: SignMap)
+    static fromDimensions(width: number, height?: number): GoBoard
 
-    get([x, y]: Vertex): number | null;
-    set([x, y]: Vertex, sign: Sign): GoBoard;
-    has([x, y]: Vertex): boolean;
-    clear(): GoBoard;
+    get([x, y]: Vertex): number | null
+    set([x, y]: Vertex, sign: Sign): GoBoard
+    has([x, y]: Vertex): boolean
+    clear(): GoBoard
     makeMove(
         sign: Sign,
         vertex: Vertex,
@@ -34,42 +34,42 @@ declare class GoBoard {
             preventOverwrite,
             preventKo,
         }?: {
-            preventSuicide?: boolean;
-            preventOverwrite?: boolean;
-            preventKo?: boolean;
+            preventSuicide?: boolean
+            preventOverwrite?: boolean
+            preventKo?: boolean
         }
-    ): GoBoard;
+    ): GoBoard
     analyzeMove(
         sign: Sign,
         vertex: Vertex
     ): {
-        pass: boolean;
-        overwrite: boolean;
-        capturing: boolean;
-        suicide: boolean;
-        ko: boolean;
-    };
-    getCaptures(sign: Sign): number;
-    setCaptures(sign: Sign, mutator: number | ((m: number) => number)): GoBoard;
-    isSquare(): boolean;
-    isEmpty(): boolean;
-    isValid(): boolean;
-    getDistance([x1, y1]: Vertex, [x2, y2]: Vertex): number;
-    getNeighbors(vertex: Vertex): Vertex[];
+        pass: boolean
+        overwrite: boolean
+        capturing: boolean
+        suicide: boolean
+        ko: boolean
+    }
+    getCaptures(sign: Sign): number
+    setCaptures(sign: Sign, mutator: number | ((m: number) => number)): GoBoard
+    isSquare(): boolean
+    isEmpty(): boolean
+    isValid(): boolean
+    getDistance([x1, y1]: Vertex, [x2, y2]: Vertex): number
+    getNeighbors(vertex: Vertex): Vertex[]
     getConnectedComponent(
         vertex: Vertex,
         predicate: (v: Vertex) => boolean,
         result?: Vertex[]
-    ): Vertex[];
-    getChain(vertex: Vertex): Vertex[];
-    getRelatedChains(vertex: Vertex): Vertex[];
-    getLiberties(vertex: Vertex): Vertex[];
-    hasLiberties(vertex: Vertex, visited?: {[key: string]: boolean}): boolean;
-    clone(): GoBoard;
-    diff(board: GoBoard): Vertex[] | null;
-    stringifyVertex(vertex: Vertex): string;
-    parseVertex(coord: string): Vertex;
-    getHandicapPlacement(count: number, {tygem}?: {tygem?: boolean}): Vertex[];
+    ): Vertex[]
+    getChain(vertex: Vertex): Vertex[]
+    getRelatedChains(vertex: Vertex): Vertex[]
+    getLiberties(vertex: Vertex): Vertex[]
+    hasLiberties(vertex: Vertex, visited?: {[key: string]: boolean}): boolean
+    clone(): GoBoard
+    diff(board: GoBoard): Vertex[] | null
+    stringifyVertex(vertex: Vertex): string
+    parseVertex(coord: string): Vertex
+    getHandicapPlacement(count: number, {tygem}?: {tygem?: boolean}): Vertex[]
 }
 
-export default GoBoard;
+export default GoBoard
--- src/main.d.ts
+++ src/main.d.ts
@@ -1,4 +1,4 @@
-import GoBoard, {Vertex, Sign, SignMap} from './GoBoard';
+import GoBoard, {Vertex, Sign, SignMap} from './GoBoard'
 
-export {Vertex, Sign, SignMap};
-export default GoBoard;
+export {Vertex, Sign, SignMap}
+export default GoBoard
