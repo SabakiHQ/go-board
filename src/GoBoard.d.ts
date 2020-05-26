8450f4978155969fa4f78958dbf6fb702b6ebeb4
Yùjǐn Lín <abc1236762@outlook.com>
2020-05-26 21:28:49 +0800

    Typescript Support (#3)
    * Typescript support
    
    * typescript support
    
    * typescript support
    
    * fix typo
    
    * small fix
    
    * No semicolons
    
    * arg name follow readme
    
    * remove useless
    
    * [x,y] to vertex

---
+++ src/GoBoard.d.ts
@@ -0,0 +1,61 @@
+export type Vertex = [number, number]
+export enum Sign {White = -1, Empty = 0, Black = 1}
+export type SignMap = Sign[][]
+
+declare class GoBoard {
+    constructor(signMap?: SignMap)
+    static fromDimensions(width: number, height?: number): GoBoard
+
+    signMap: SignMap
+    height: number
+    width: number
+
+    get(vertex: Vertex): number | null
+    set(vertex: Vertex, sign: Sign): GoBoard
+    has(vertex: Vertex): boolean
+    clear(): GoBoard
+    makeMove(
+        sign: Sign,
+        vertex: Vertex,
+        options?: {
+            preventSuicide?: boolean
+            preventOverwrite?: boolean
+            preventKo?: boolean
+        }
+    ): GoBoard
+    analyzeMove(
+        sign: Sign,
+        vertex: Vertex
+    ): {
+        pass: boolean
+        overwrite: boolean
+        capturing: boolean
+        suicide: boolean
+        ko: boolean
+    }
+    getCaptures(sign: Sign): number
+    setCaptures(
+        sign: Sign,
+        mutator: number | ((prevCaptures: number) => number)
+    ): GoBoard
+    isSquare(): boolean
+    isEmpty(): boolean
+    isValid(): boolean
+    getDistance(vertex1: Vertex, vertex2: Vertex): number
+    getNeighbors(vertex: Vertex): Vertex[]
+    getConnectedComponent(
+        vertex: Vertex,
+        predicate: (vertex: Vertex) => boolean
+    ): Vertex[]
+    getChain(vertex: Vertex): Vertex[]
+    getRelatedChains(vertex: Vertex): Vertex[]
+    getLiberties(vertex: Vertex): Vertex[]
+    hasLiberties(vertex: Vertex): boolean
+    clone(): GoBoard
+    diff(board: GoBoard): Vertex[] | null
+    stringifyVertex(vertex: Vertex): string
+    parseVertex(coord: string): Vertex
+    getHandicapPlacement(count: number): Vertex[]
+}
+
+export default GoBoard
---
+++ src/main.d.ts
@@ -0,0 +1,4 @@
+import GoBoard, {Vertex, Sign, SignMap} from './GoBoard'
+
+export {Vertex, Sign, SignMap}
+export default GoBoard
